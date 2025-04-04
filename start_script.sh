be737b1c25569e378594971d444da57d2da7d27d
Bucky0719 <100702108+Bucky0719@users.noreply.github.com>
2025-04-04 15:29:14 +0530

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Set your AWS region and EC2 instance ID
+# Set AWS region and EC2 instance ID
 AWS_REGION="us-east-1"  # Change to your AWS region
 INSTANCE_ID="i-0c1e67d0a65bee739"  # Replace with your EC2 instance ID
 
-# Start the EC2 instance
+# Start EC2 instance
 echo "Starting EC2 instance: $INSTANCE_ID..."
 aws ec2 start-instances --instance-ids $INSTANCE_ID --region $AWS_REGION
 
-# Wait for the instance to start
+# Wait for instance to start
 echo "Waiting for the instance to start..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID --region $AWS_REGION
 
-# Get the public IP address of the instance
+# Get the public IP address
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $AWS_REGION --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
+# Print the IP (For Debugging)
 echo "EC2 instance is now running."
 echo "Public IP Address: $PUBLIC_IP"
+
+# Export Public IP as Azure DevOps Variable
+echo "##vso[task.setvariable variable=EC2_PUBLIC_IP;]$PUBLIC_IP"
