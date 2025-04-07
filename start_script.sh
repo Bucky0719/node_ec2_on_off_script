11485c36d5f92c057c1459e4d6f4fc3a059343d0
Bucky0719 <100702108+Bucky0719@users.noreply.github.com>
2025-04-07 11:03:15 +0530

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -13,14 +13,10 @@ echo "Waiting for the instance to start..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID --region $AWS_REGION
 
 # Get the public IP address
-PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $AWS_REGION --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+EC2_PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $AWS_REGION --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
-if [ -z "$PUBLIC_IP" ]; then
-  echo "Failed to get public IP!"
-  exit 1
-fi
 
-echo "EC2 Public IP: $PUBLIC_IP"
+echo "EC2 Public IP: $EC2_PUBLIC_IP"
 
 # Output to pipeline
-echo "##vso[task.setvariable variable=EC2_PUBLIC_IP;isOutput=true]$PUBLIC_IP"
+echo "##vso[task.setvariable variable=EC2_PUBLIC_IP;isOutput=true]$EC2_PUBLIC_IP"
