70f03590a39ef002dfc49df6d7f15d9a40331e2e
Ubuntu <ubuntu@ip-172-31-84-189.ec2.internal>
2025-04-22 08:57:55 +0000

    first commit

--- start_script.sh
+++ start_script.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-AWS_REGION="us-east-1"  
-INSTANCE_ID="i-0c1e67d0a65bee739"  
+echo "🔄 Starting EC2 instance: $INSTANCE_ID"
 
-echo "Starting EC2 instance: $INSTANCE_ID..."
-aws ec2 start-instances --instance-ids $INSTANCE_ID --region $AWS_REGION
+aws ec2 start-instances --instance-ids "$INSTANCE_ID" --region "$AWS_DEFAULT_REGION"
+aws ec2 wait instance-running --instance-ids "$INSTANCE_ID" --region "$AWS_DEFAULT_REGION"
 
-echo "Waiting for the instance to start..."
-aws ec2 wait instance-running --instance-ids $INSTANCE_ID --region $AWS_REGION
+echo "✅ EC2 instance started successfully."
 
-EC2_PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --region $AWS_REGION --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
-
-echo "EC2_Public_IP: $EC2_PUBLIC_IP"
-echo "##vso[task.setvariable variable=EC2_PUBLIC_IP;isOutput=true]$EC2_PUBLIC_IP"
--- stop_script.sh
+++ stop_script.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Set your AWS region and EC2 instance ID
-AWS_REGION="us-east-1"
-INSTANCE_ID="i-0c1e67d0a65bee739"
+echo "🔻 Stopping EC2 instance: $INSTANCE_ID"
 
-# Stop the EC2 instance
-echo "Stopping EC2 instance: $INSTANCE_ID..."
-aws ec2 stop-instances --instance-ids $INSTANCE_ID --region $AWS_REGION
+aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region "$AWS_DEFAULT_REGION"
+aws ec2 wait instance-stopped --instance-ids "$INSTANCE_ID" --region "$AWS_DEFAULT_REGION"
 
-# Wait for the instance to stop
-echo "Waiting for the instance to stop..."
-aws ec2 wait instance-stopped --instance-ids $INSTANCE_ID --region $AWS_REGION
+echo "✅ EC2 instance stopped successfully."
 
-echo "EC2 instance $INSTANCE_ID has been stopped."
